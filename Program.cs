cd9d412062dc21f302d09030e7f1e9538bf98b2e
mikhail_tseiklin <mikaeltseiklin@yandex.ru>
2022-08-02 22:57:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*  Напишите метод, который отображает квадрат из некоторого символа. 
+ * Метод принимает в качестве параметра: длину стороны квадрата, символ.
+ */
+namespace Task4
+{
+    class Program
+    {
+        static void PrintSquare(int side, char sign)
+        {
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    Console.Write($" {sign} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите сторону квадрата: ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Введите произвольный символ: ");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+            PrintSquare(lenght, sign);
+        }
+    }
+}
